db644a0fef47af655503b841197dbf224745b2ef
Liu <binbin.liu@dell.com>
2024-12-26 11:49:00 -0500

    adding the contact page with sending email function

--- pages/contact.py
+++ pages/contact.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from send_email import send_email
 
+st.set_page_config(layout="wide")
 
 st.header("Contact Us")
+
+with st.form(key="contact_form"):
+    user_email=st.text_input("Your email address")
+    message_box = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{message_box}
+"""
+    button = st.form_submit_button("Submit") 
+
+    if button:
+       
+        send_email(message)
+
+        st.info("Your email was sent successfully!")
